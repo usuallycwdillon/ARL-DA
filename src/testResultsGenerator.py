6eaba456cd63dbba56ebb3fa1ac33d23fbc69a66
usuallycwdillon <cwdillon@gmail.com>
2016-04-26 12:45:36 -0400

    Corrections for scoring. THIS is the good version. Includes an 'OverallScore' for easier data extraction.

--- src/testResultsGenerator.py
+++ src/testResultsGenerator.py
@@ -5,10 +5,7 @@ from numpy.random import random_sample
 import random
 import copy
 import pandas as pd
-import scipy.stats as st
-import scipy as sc
 import json
-from itertools import chain
 from time import gmtime, strftime
 
 
@@ -28,7 +25,6 @@ class surveyDataGenerator(object):
         learners is a list of learners so that the survey is keyed to learners
         """
         super(surveyDataGenerator, self).__init__()
-
 
         #Read all of the learner profiles from the class
         def readClassProfiles(json_file):
@@ -84,12 +80,16 @@ class surveyDataGenerator(object):
             data = getSurveyData(pages)
             survey_responses.append(data)
 
+        #print survey_responses[0]['SurveyName'], " is at index 0"
+
+
         # print survey_responses
         """
         The final value of 'survey_response' is a list containing two survey data dicts: one for the pre-lesson survey
         and one for the post-lesson survey. Construction of the responses should copy the list and iterate over the
         two survey responses and adding simulated responses.
         """
+
 
 
     # Raghav's stat functions from the data_simulation script to shape the simulated data distributions
@@ -101,26 +101,61 @@ class surveyDataGenerator(object):
         def rand_prob(n):
             return np.random.random((n)).tolist()
 
-    # Recalculate Marksmanship based on heavy skurtosis in normal dist
-        def calculateProbabilities(input_int):
-            if input_int == 0:
-                temp = rand_prob(1)[0]
-                if temp < 0.5:
-                    return 0
-                else:
-                    return 1
-            elif input_int == 1:
-                outcomes = [0,1]
-                probs = [0.3, 0.7]
-                return weighted_probs(outcomes, probs, 1)
-            else:
-                outcomes = [0,1]
-                probs = [0.2, 0.8]
-                return weighted_probs(outcomes, probs, 1)
 
-        def fillInAnswer(answer_matrix, actual_matrix, marksman_background):
-            prob_correctness = calculateProbabilities(marksman_background)
-            print prob_correctness
+        def calculateProbabilities(rating, post):
+            rankings = {"Unexperienced":1, "Novice":2, "Marksman":3, "Sharpshooter":4, "Expert":5}
+            if rating == "Unexperienced":
+                temp = rand_prob(1)[0]
+                if post !=1:
+                    if temp < 0.75:
+                        return 0
+                    else:
+                        return 1
+                else:
+                    if temp < 0.375:
+                        return 0
+                    else:
+                        return 1
+            elif rating == "Novice":
+                if post != 1:
+                    outcomes = [0, 1]
+                    probs = [0.45, 0.55]
+                    return weighted_probs(outcomes, probs, 1)
+                else:
+                    outcomes = [0, 1]
+                    probs = [0.4, 0.6]
+                    return weighted_probs(outcomes, probs, 1)
+            elif rating == "Marksman":
+                if post != 1:
+                    outcomes = [0, 1]
+                    probs = [0.35, 0.65]
+                    return weighted_probs(outcomes, probs, 1)
+                else:
+                    outcomes = [0, 1]
+                    probs = [0.3, 0.7]
+                    return weighted_probs(outcomes, probs, 1)
+            elif rating == "Sharpshooter":
+                if post != 1:
+                    outcomes = [0, 1]
+                    probs = [0.25, 0.75]
+                    return weighted_probs(outcomes, probs, 1)
+                else:
+                    outcomes = [0, 1]
+                    probs = [0.2, 0.8]
+                    return weighted_probs(outcomes, probs, 1)
+            else:
+                if post != 1:
+                    outcomes = [0, 1]
+                    probs = [0.15, 0.85]
+                    return weighted_probs(outcomes, probs, 1)
+                else:
+                    outcomes = [0, 1]
+                    probs = [0.1, 0.9]
+                    return weighted_probs(outcomes, probs, 1)
+
+        def fillInAnswer(answer_matrix, actual_matrix, marksman_background, survey_index):
+            prob_correctness = calculateProbabilities(marksman_background, survey_index)
+            #print prob_correctness
             if prob_correctness == 0:
                 # randomly select the index where it is not the right answer
                 ans_pos = [i for i,x in enumerate(actual_matrix) if x != 0]
@@ -156,28 +191,29 @@ class surveyDataGenerator(object):
 
 
         def constructResponses():
-            pre_test_responses, post_test_responses = [], []
             learner_responses = []
 
             # iterate over each learner
             for i in range(len(self.learners.columns)):
                 this_response = copy.deepcopy(survey_responses)
-                for s in this_response:
+                for si, s in enumerate(this_response):
                     s['LearnerID'] = self.learners[i]['email_id']
-
+                    overall_points = []
                     for so in s['SurveyObjects']:
-                        so['Answer'] = fillInAnswer(so['Answer'], so['Scoring'], self.learners[i]['qualification_performance'])
+                        so['Answer'] = fillInAnswer(so['Answer'], so['Scoring'], self.learners[i]['qualification_performance'], si)
                         so['Points'] = calculateScore(so['Answer'], so['Scoring'])
+                        overall_points.append(so['Points'])
+                    s['OverallScore'] = sum(overall_points)
 
                 learner_responses.append(this_response)
             print "the learner responses list is ", len(learner_responses)
 
             for lr in learner_responses:
                 with open("../data/pre_test_responses/" + lr[0]["LearnerID"] + ".json", "w") as data_out:
-                    json.dump(learner_responses[0], data_out)
+                    json.dump(lr[0], data_out)
 
                 with open("../data/post_test_responses/" + lr[1]["LearnerID"] + ".json", "w") as data_out:
-                    json.dump(learner_responses[1], data_out)
+                    json.dump(lr[1], data_out)
 
         constructResponses()
 
@@ -187,8 +223,8 @@ class surveyDataGenerator(object):
     {Agent: learner x, Verb: completed, Object: pre-lesson attitude survey for course abc with {Extension: responses to
     questions}, Context: JSON of survey questions and scoring rubric}
 
-    For purposes of generating data in this case, we should just generate a single JSON object with questions and answers
-    for each learner. Save the file with the: course name, testing date, learner ID. For example:
+    For purposes of generating data in this case, we should just generate a single JSON object for each learner with
+    questions and answers. Save the file with the: course name, testing date, learner ID. For example:
     'marksmanship_data_simulation-123405T_21April2016-learnerID.domain.json'
     """
 
